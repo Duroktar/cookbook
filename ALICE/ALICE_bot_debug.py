f03264bf475d94fcbbfbe7ae4a5f0b789b137f82
Duroktar <scott.doucet@hotmail.com>
2016-06-05 11:40:38 -0300

    fixed pathing

--- ALICE/ALICE_bot_debug.py
+++ ALICE/ALICE_bot_debug.py
@@ -29,7 +29,7 @@ def talk(text):
 def userInput():
     print "success"
     print "opening dictation file"
-    f = open("dictation.txt")
+    f = open( path + "/dictation.txt")
     print "success"
     print "reading dictation file into a"
     a = f.read()
@@ -49,7 +49,7 @@ def userInput():
 
 def clearInput():
     print "getting ready to set dictation file to false"
-    f = open('dictation.txt', 'w')
+    f = open( path + '/dictation.txt', 'w')
     print "opened file"
     f.write("False")
     print "reset to false"
@@ -65,7 +65,7 @@ def writeHistory(i):
     ts = ""
     print "success"
     print "opening history file"
-    f = open('history.txt', 'a')
+    f = open( path + '/history.txt', 'a')
     print "success"
     print "getting timestamp"
     ts = time.time()
