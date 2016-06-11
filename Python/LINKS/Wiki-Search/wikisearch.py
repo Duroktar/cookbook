8b43025a4b0993ca8ce08ad42e8a34772dba3837
traBpUkciP <scott.doucet@hotmail.com>
2016-06-11 14:31:02 -0300

    working beta 0.1

--- Python/LINKS/Wiki-Search/wikisearch.py
+++ Python/LINKS/Wiki-Search/wikisearch.py
@@ -5,66 +5,71 @@ import datetime
 import time
 import sys
 import os
-import json
-import flatdict
 import ctypes
+import wikipedia
+import ConfigParser
 
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 QUERY = ""
+CONFIRM = False
+SECTION_COUNT = 0
+Config = ConfigParser.ConfigParser()
+Config.read(PATH + "\config.ini")
+Config.sections()
+PORT = Config.get("LINKS", 'Port')
+WEBKEY = Config.get("LINKS", 'Key')
+
+
+def get_confirm(self):
+    global CONFIRM
+    return self
 
 
 def get_input():
     global QUERY
-    print "Opening dictation.txt"
+    # print "Opening dictation.txt"
     f = open(PATH + "\dictation.txt")
-    print "Got it.."
+    # print "Got it.."
     x = f.read()
-    print "Reading search string.."
-    print "Search string: " + x + "Closing dictation.txt"
+    # print "Reading search string.."
+    # print "Search string: " + x + "Closing dictation.txt"
     f.close()
     QUERY = x
     return x
 
 
-def build_url(_phrase):
-    print "---------------"
-    print "Building URL"
-    phrase = _phrase.replace(" ", "+")
-    url = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1" \
-          "&prop=extracts&exintro=&explaintext=&titles=_replace_"
-    w_url = url.replace("_replace_", phrase)
-    print "Url: " + w_url + "----------------"
-    return w_url
-
-
-def strip_non_ascii(string):
-    """ Returns the string without non ASCII characters """
-    stripped = (c for c in string if 0 < ord(c) < 127)
-    return ''.join(stripped)
-
-
-def get_json(url):
-    w_url = url
-    print "Sending request"
-    print "Request: " + w_url
-    data = urllib.urlopen(w_url).read()
-    print type(data)
-    # print "Response: " + data
-    print "--------------------------------------"
-    print "Converting to json"
-    json_data = json.loads(data)
-    print type(json_data)
-    print "--------------------------------------"
-    flat = flatdict.FlatDict(json_data)
-    for key in flat:
-        if "extract" in key:
-            x = key
-            key = flat[x]
-            answer = strip_non_ascii(key)
-            return answer
+def search_wiki(text):
+    x = wikipedia.search(text)
+    # print x
+    # print x[0]
+    y = wikipedia.page(x[0])
+    # print y
+    title = y.title
+    sections = y.sections
+    summary = y.summary
+    # print "Search results:"
+    # print x
+    # print "------------------------------------------------------------------------------------------------"
+    # print "Title: "
+    # print title
+    # print "--------------------------------------------------------------------------------------------------"
+    # print "Sections: The new way.."
+    # print type(sections)
+    for i in sections:
+        global SECTION_COUNT
+        x = y.section(i)
+        if x == "":
+            continue
         else:
-            return "No match found for, " + QUERY
-    exit()
+            # print "----------------------------------------------------------------------------------------------"
+            # print "{}: ".format(i)
+            # print x.strip()
+            # print type(x)
+            SECTION_COUNT += 1
+            continue
+    # print "------------------------------------------------------------------------------------------------"
+    # print "Summary: "
+    return summary
 
 
 def write_history(i):
@@ -72,34 +77,54 @@ def write_history(i):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     a = st + ": " + i
-    print a
+    # print a
     f.write(a)
     f.write('\n')
     f.close()
 
 
-def talk(text):
+def talk(port, webkey, text):
     # THIS URL NEEDS TO BE SET TO YOUR PORT AND KEY ACCORDINGLY
     # THIS PART ONLY WORK IF YOU HAVE LINKS WEB REQUEST SETTINGS ON DEFAULT
     try:
-        url = 'http://127.0.0.1:54657/?action=[Speak("placeholder")]&key=1234ABC&request=enable'
-        newurl = url.replace("placeholder", text)  # fill in text to be spoken
-        urllib.urlopen(newurl)
+        url = 'http://127.0.0.1:_port_/?action=[Speak("_placeholder_")]&key=_passkey_&request=enable'
+        addport = url.replace("_port_", port)
+        addkey = addport.replace("_passkey_", webkey)
+        finalurl = addkey.replace("_placeholder_", text)  # fill in text to be spoken
+        print finalurl
+        urllib.urlopen(finalurl)
     except IOError:
-        ctypes.windll.user32.MessageBoxA(0, "CHECK YOUR WEB SERVER SETTING IN LINKS", "Connection error", 1)
+        talk(PORT, WEBKEY, "LINKS web server not accepting request. "
+                           "Check port and key settings in user options under web settings.")
+        ctypes.windll.user32.MessageBoxA(0, "Check LINKS web settings under user options. "
+                                            "Make sure port & key match your config file.")
+
+
+def strip_non_ascii(string):
+    """ Returns the string without non ASCII characters """
+    stripped = (c for c in string if 0 < ord(c) < 127)
+    return ''.join(stripped)
+
+def strip_bad_chars(string):
+    bad_chars = '"=:;/{}<>'
+    a = "".join(c for c in string if c not in bad_chars)
+    return a
 
 
 def main():
-    time.sleep(.3)
-    print "Starting LINKS/Wiki engine.."
-    print "Success.."
-    print "----------------------------"
-    print "Getting User Input"
-    data = get_input()
-    url = build_url(data)
-    resp = get_json(url)
-    print "Response: " + resp
-    talk(resp)
+    time.sleep(.15)
+    # print "Starting LINKS/Wiki engine.."
+    # print "----------------------------"
+    initial = get_input()
+    search = search_wiki(initial)
+    strip = strip_non_ascii(search)
+    clean = strip_bad_chars(strip)
+    answer = str(clean)
+    # print answer
+    # print type(answer)
+    # print "==============================="
+    talk(PORT, WEBKEY, answer)
 
 
+#  talk(PORT, WEBKEY, "hello")
 main()
