99ca7c2dd58083bad2e21f8bbdb4ad4a0de44ea8
traBpUkciP <scott.doucet@hotmail.com>
2016-06-11 21:10:33 -0300

    updated script with config file

--- Python/LINKS/Movie-Search/movieSearch.py
+++ Python/LINKS/Movie-Search/movieSearch.py
@@ -7,31 +7,38 @@ import sys
 import os
 import json
 import ctypes
+import ConfigParser
+
 
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 SEARCH = ""
+Config = ConfigParser.ConfigParser()
+Config.read(PATH + "\config.ini")
+Config.sections()
+PORT = Config.get("LINKS", 'Port')
+WEBKEY = Config.get("LINKS", 'Key')
+
 
 def getInput():
-
-    print "Opening dictation.txt"
-    f = open(PATH + "\dictation.txt")
-    print "Got it.."
-    x = f.read()
-    print "Reading search string.."
-    print "Search string: " + x
-    f.close()
     global SEARCH
+    # print "Opening dictation.txt"
+    f = open(PATH + "\dictation.txt")
+    # print "Got it.."
+    x = f.read()
+    # print "Reading search string.."
+    # print "Search string: " + x
+    f.close()
     SEARCH = x
     return x
 
 
 def buildUrl(x):
     phrase = x.replace(" ", "+")
-    print "---------------"
-    print "Building URL"
+    # print "---------------"
+    # print "Building URL"
     url = "http://www.omdbapi.com/?t=_replace_&y=&plot=short&r=json"
     w_url = url.replace("_replace_", phrase)
-    print "Url: " + w_url + "----------------"
+    # print "Url: " + w_url + "----------------"
     return w_url
 
 
@@ -42,17 +49,18 @@ def strip_non_ascii(string):
 
 
 def getJson(url):
+    global SEARCH
     w_url = url
-    print "Sending request"
-    print "Request: " + w_url
+    # print "Sending request"
+    # print "Request: " + w_url
     data = urllib.urlopen(w_url).read()
-    print "--------------------------------------"
-    print "Converting to json:"
+    # print "--------------------------------------"
+    # print "Converting to json:"
     jsonData = json.loads(data)
-    print jsonData
-    print "--------------------------------------"
+    # print jsonData
+    # print "--------------------------------------"
     if jsonData['Response'] == 'False':
-        return "Nothing found for {}.".format(SEARCH)
+        return "Nothing found."
     else:
         title = jsonData['Title']
         date = jsonData['Year']
@@ -60,7 +68,7 @@ def getJson(url):
         directors = jsonData['Director']
         actors = jsonData['Actors']
         plot = jsonData['Plot']
-        movie = "Released in {}, {} is an {} film directed by {} and starring {}. \
+        movie = "Released in {}, {} is a {} film directed by {} and starring {}. \
             The movie is summarized as follows. . {}".format(date, title, genre, directors, actors, plot)
         return movie
 
@@ -70,32 +78,36 @@ def writeHistory(i):
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     a = st + ": " + i
-    print a
+    # print a
     f.write(a)
     f.write('\n')
     f.close()
 
 
-def talk(text):
+def talk(port, webkey, text):
     # THIS URL NEEDS TO BE SET TO YOUR PORT AND KEY ACCORDINGLY
     # THIS PART ONLY WORK IF YOU HAVE LINKS WEB REQUEST SETTINGS ON DEFAULT
     try:
-        url = 'http://127.0.0.1:54657/?action=[Speak("placeholder")]&key=1234ABC&request=enable'
-        newurl = url.replace("placeholder", text)  # fill in text to be spoken
-        urllib.urlopen(newurl)
+        url = 'http://127.0.0.1:_port_/?action=[Speak("_placeholder_")]&key=_passkey_&request=enable'
+        addport = url.replace("_port_", port)
+        addkey = addport.replace("_passkey_", webkey)
+        finalurl = addkey.replace("_placeholder_", text)  # fill in text to be spoken
+        # print finalurl
+        urllib.urlopen(finalurl)
     except IOError:
-        print "LINKS web server not accepting request. Check port and key settings in user options under web settings."
-        ctypes.windll.user32.MessageBoxA(0, "Check LINKS web settings under user options. Make sure port:54657 & key:1234ABC match. Copy and paste this into your browser afterwards to see if they're enabled. http://127.0.0.1:54657/?action=[Speak('Test')]&key=1234ABC&request=enable", "Can't communicate with LINKS!", 1)
+        ctypes.windll.user32.MessageBoxA(0, "Check LINKS web settings under user options. "
+                                            "Make sure port & key match your config file.")
+
 
 def main():
     time.sleep(.3)
-    print "Starting LINKS/Wiki engine.."
-    print "Success.."
-    print "----------------------------"
-    print "Getting User Input"
+    # print "Starting LINKS/Wiki engine.."
+    # print "Success.."
+    # print "----------------------------"
+    # print "Getting User Input"
     data = getInput()
     url = buildUrl(data)
     resp = getJson(url)
-    talk(resp)
+    talk(PORT, WEBKEY, resp)
 
 main()
--- Python/LINKS/Movie-Search/readme.txt
+++ Python/LINKS/Movie-Search/readme.txt
@@ -1,3 +1,16 @@
 Get movie information from the web and have Jarvis read you a summary.
 
+1. Unpack contents into folder on your computer and keep note of the path for instruction #3.
+2. Open config.ini file in notepad and set port and key for LINKS Web Service.
+3. Enter this command in LINKS under SHELL -
+  Command: look up movie {that=test_dictation}
+  Response: 
+  Action: -hi1CMD /m /c "echo {that} > **PATH_TO_INSTALL_FOLDER**\dictation.txt | python.exe **PATH_TO_INSTALL_FOLDER**\movieSearch.py"
+  Profile: main
+4. Have fun :)
+
+
+Feel free to e-mail me with any questions or if you need help setting it up @ duroktar@gmail.com
+
+
 By: traBpUkciP
