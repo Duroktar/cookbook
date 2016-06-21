65e7e3a1ac9332a7627b4dfd2a9a5e6a12452a10
traBpUkciP <scott.doucet@hotmail.com>
2016-06-20 21:39:36 -0300

    moved

--- Python/LINKS/Wiki-Search/wikisearch.py
+++ Python/LINKS/Wiki-Search/wikisearch.py
@@ -103,6 +103,7 @@ def strip_non_ascii(string):
     stripped = (c for c in string if 0 < ord(c) < 127)
     return ''.join(stripped)
 
+
 def strip_bad_chars(string):
     bad_chars = '"=:;/{}<>'
     a = "".join(c for c in string if c not in bad_chars)
--- Python/api.ai/Testing/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import unittest
-
-from .test_actions import *
-
-if __name__ == '__main__':
-    unittest.main()
