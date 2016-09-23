fc8e129e0da7f881945200b8e58e7c83942fe5ef
traBpUkciP <scott.doucet@hotmail.com>
2016-09-23 02:44:51 -0300

    Update linksapiai.py

--- Python/LINKS/Api-AI/linksapiai.py
+++ Python/LINKS/Api-AI/linksapiai.py
@@ -1,66 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import links
-import sys
+import os, sys
 import json
-import os
 import ConfigParser
-from iniparse import INIConfig
-import apiai
+try:
+    import apiai
+except ImportError:
+    print "Starting initial setup."
+    import subprocess
+    FNULL = open(os.devnull, 'w')
+    retcode = subprocess.call(['pip', 'install', 'apiai'], stdout=FNULL, stderr=subprocess.STDOUT)
+    import apiai
 
+QUERY = " ".join(sys.argv[1:])
 
 PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-Config = ConfigParser.ConfigParser()
-Config.read(PATH + "\config.ini")
-Config.sections()
-PORT = Config.get("LINKS", 'Port')
-WEBKEY = Config.get("LINKS", 'Key')
-CLIENTKEY = Config.get("APIKey", 'ClientKey')
-DEVKEY = Config.get("APIKey", 'DevKey')
-SESSION_ID = Config.get("APIKey", 'SessionID')
-
+try:
+    Config = ConfigParser.ConfigParser()
+    Config.read(PATH + "\config.ini")
+    Config.sections()
+    CLIENTKEY = Config.get("APIKey", 'clientkey')
+    SESSION_ID = Config.get("APIKey", 'sessionid')
+except Exception:
+    print """There was a problem loading your config file. 
+             Check that the file actually exists and
+             that your a.p.i. key is entered properly."""
 
 def main():
     ai = apiai.ApiAI(CLIENTKEY)
     request = ai.text_request()
     if SESSION_ID != "":
-        print "Session ID: " + SESSION_ID
         request.session_id = SESSION_ID
-    request.query = links.varfetch("LastSubject")
-    links.write_history(request.query)
+    request.lang = 'en'
+    request.query = QUERY
     response = request.getresponse()
     new_response = response.read()
     jresponse = json.loads(new_response)
-
     try:
         if jresponse['status']['errorDetails']:
-            print "Your api client key is invalid. Please check your settings in the config file."
+            print "Your api client key is invalid. Please check your config file."
             return
     except:
         if SESSION_ID == "":
             key = jresponse['id']
-            cfg = INIConfig(open(PATH + "\config.ini"))
-            cfg.APIKey.SessionID = " " + key
-            f = open(PATH + "\config.ini", 'w')
-            print >> f, cfg
-            f.close()
-        if jresponse['result']['fulfillment']['speech'] == "":
-            links.talk(PORT, WEBKEY, "Nothing found for that query.")
-            print "Nothing found for that query."
-            return
-        else:
-            response = jresponse['result']['fulfillment']['speech']
-            final = strip_non_ascii(response)
-            links.talk(PORT, WEBKEY, final)
-            print final
-
+            Config.set("APIKey", 'SessionID', key)
+            with open(PATH + "\config.ini", 'w') as configfile:
+                Config.write(configfile)
+    if jresponse['result']['fulfillment']['speech'] == "":
+        print "I don't have an answer for that"
+        quit()
+    else:
+        response = jresponse['result']['fulfillment']['speech']
+        print response.replace(';', ',').replace('"', "'").encode('utf-8')
 
 def strip_non_ascii(string):
     """ Returns the string without non ASCII characters """
     stripped = (c for c in string if 0 < ord(c) < 127)
     return ''.join(stripped)
-
 
 if __name__ == '__main__':
     main()
