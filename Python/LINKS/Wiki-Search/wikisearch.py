2f8ee855c7037c9fbd02df9c4a43edcaabf0c904
traBpUkciP <scott.doucet@hotmail.com>
2016-09-23 00:50:17 -0300

    Add files via upload

---
+++ Python/LINKS/Wiki-Search/wikisearch.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import os, sys
+import time
+from thread import start_new_thread
+try:
+    import wikipedia
+except ImportError:
+    print "Starting initial setup."
+    import subprocess
+    FNULL = open(os.devnull, 'w')
+    retcode = subprocess.call(['pip', 'install', 'wikipedia'], stdout=FNULL, stderr=subprocess.STDOUT)
+    import wikipedia
+    
+query = sys.argv[1:]
+
+def main():
+    time.sleep(.05)
+    result = search_wiki(query)
+    print(result)
+
+def search_wiki(text):
+    SECTION_COUNT = 0
+    x = wikipedia.search(text)
+    y = wikipedia.page(x[0])
+    summary = y.summary.replace(';', ',').replace('"', "'").encode('utf-8')
+    return summary    
+    
+if __name__ == '__main__':
+    main()
+
